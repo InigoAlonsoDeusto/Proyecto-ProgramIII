f4e0e69a77cabe4a0b54473301ad0514fa2d7bcf
Adrian Martinez <adrianmartinez@opendeusto.es>
2022-11-13 13:17:19 +0100

    Creacion de la funcion random y array wombats

--- WombatWarsProyecto/src/Juego/WombatWarsMain.java
+++ WombatWarsProyecto/src/Juego/WombatWarsMain.java
@@ -9,11 +9,17 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
+import java.util.Random;
+
+
 public class WombatWarsMain extends JFrame {
 
      JPanel panel;
      JLabel[] agujeros = new JLabel[9];
      Border borde = BorderFactory.createLineBorder(new Color (0, 100, 0), 5); //temporal para marcar los agujeros mientras no tengamos imagenes
+
+     private int[] agujeroConWombat = new int[9]; //Array que va a representar el estado del agujero
+                                                  //Si int=1 el wombat se verá, si int=0 no.
 
     
     public static  void main(String[] args) {
@@ -93,7 +99,29 @@ public class WombatWarsMain extends JFrame {
         agujeros[8].setBounds(900, 275,100,100);
         panel.add(agujeros[8]);
 
+
+        for(int i = 0; i < 9; i++) {
+            agujeroConWombat[i] = 0; //Inicializamos todos los agujeros a 0 para que no haya ningun wombat visible.
+        }
+
         
+         
+    }
+
+    public void randomWombat() //Esta clase va a generar un numero aleatorio (del 0 al 9 por los agujeros)
+                               //para determinar el nº del hoyo en el que saldrá el wombat.
+    { 
+
+        Random numeroRandom = new Random(System.currentTimeMillis()); //Inicializamos el numeroRandom
+        int wombat = numeroRandom.nextInt(9); //Creamos la variable wombat (para luego poder saber cual es el que sale)
+                                                      //Y le ponemos el numero random que va a crear este metodo entre el 0 y 9.
+
+        agujeroConWombat[wombat] = 1; //Ponemos el agujero a 1 para que el wombat esté visible (salga del agujero).
+
+        agujeros[wombat].setBackground(new Color (100, 0, 0)); //Lo pintamos por ahora para comprobar que sale, aquí le meteremos la imagen del wombat con .setIcon().
+
+
+
     }
 
 
