626df0f86864e0e6f17202d98b569c86213f861c
Alfonsacion <igoalonso@gmail.com>
2022-11-13 02:24:50 +0100

    Vetana, Agujeros

--- WombatWarsProyecto/src/Juego/WombatWarsMain.java
+++ WombatWarsProyecto/src/Juego/WombatWarsMain.java
@@ -1,5 +1,99 @@
 package Juego;
 
-public class WombatWarsMain {
+
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+public class WombatWarsMain extends JFrame {
+
+     JPanel panel;
+     JLabel[] agujeros = new JLabel[9];
+     Border borde = BorderFactory.createLineBorder(new Color (0, 100, 0), 5); //temporal para marcar los agujeros mientras no tengamos imagenes
+
+    
+    public static  void main(String[] args) {
+        WombatWarsMain ventana = new WombatWarsMain(); //Las  dos siguientes lineas llaman y hacen visible la ventana.
+        ventana.setVisible(true);
+
+    }
+
+    public WombatWarsMain() {
+        setTitle("WombatWars"); //Las siguientes 4 lineas determinan algunas caracteristicas de la ventana emergente; su nombre, tamaño y su manera de cerrarse.
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 1200, 650);
+
+        JPanel PanelWW =  new JPanel(); //Estas 3 lineas son para poder meter cosas a nuestra ventana, y de paso ponerla verde
+        PanelWW.setBackground(new Color (0, 100, 0)); //Esto hay que cambiarlo por texturas mas adelante que queda biendefeo
+        PanelWW.setLayout(null);
+        setContentPane(PanelWW);
+
+        panel = new JPanel(); //JPanel dentro de JPanel especifico para las cajas de las que podran salir los wombats
+        panel.setBackground(new Color(0, 50, 0));
+        panel.setBounds(0, 200, 1200, 450);
+        panel.setLayout(null);
+        PanelWW.add(panel);
+
+        agujeros[0] = new JLabel("0"); //A continuacion los Jlabels de las cajas de las que podran salir los wombats, falta poner imagenes como en el fondo
+        agujeros[0].setBorder(borde);
+        agujeros[0].setName("0");
+        agujeros[0].setBounds(300, 25, 100,100);
+        panel.add(agujeros[0]);
+
+        agujeros[1] = new JLabel("1");
+        agujeros[1].setBorder(borde);
+        agujeros[1].setName("1");
+        agujeros[1].setBounds(600, 25,100,100);
+        panel.add(agujeros[1]);
+
+        agujeros[2] = new JLabel("2");
+        agujeros[2].setBorder(borde);
+        agujeros[2].setName("2");
+        agujeros[2].setBounds(900, 25,100,100);
+        panel.add(agujeros[2]);
+
+        agujeros[3] = new JLabel("3");
+        agujeros[3].setBorder(borde);
+        agujeros[3].setName("3");
+        agujeros[3].setBounds(300, 150,100,100);
+        panel.add(agujeros[3]);
+
+        agujeros[4] = new JLabel("4");
+        agujeros[4].setBorder(borde);
+        agujeros[4].setName("4");
+        agujeros[4].setBounds(600, 150,100,100);
+        panel.add(agujeros[4]);
+
+        agujeros[5] = new JLabel("5");
+        agujeros[5].setBorder(borde);
+        agujeros[5].setName("5");
+        agujeros[5].setBounds(900, 150,100,100);
+        panel.add(agujeros[5]);
+
+        agujeros[6] = new JLabel("6");
+        agujeros[6].setBorder(borde);
+        agujeros[6].setName("6");
+        agujeros[6].setBounds(300, 275,100,100);
+        panel.add(agujeros[6]);
+
+        agujeros[7] = new JLabel("7");
+        agujeros[7].setBorder(borde);
+        agujeros[7].setName("7");
+        agujeros[7].setBounds(600, 275,100,100);
+        panel.add(agujeros[7]);
+
+        agujeros[8] = new JLabel("8");
+        agujeros[8].setBorder(borde);
+        agujeros[8].setName("8");
+        agujeros[8].setBounds(900, 275,100,100);
+        panel.add(agujeros[8]);
+    }
+
+
 
 }
