4a11a8839cdb5d9fc403ea85390b2c4880039396
Adrian Martinez <adrianmartinez@opendeusto.es>
2022-11-27 22:03:26 +0100

    Metodo recursivo

--- WombatWarsProyecto/src/Juego/Launcher/Launcher.java
+++ WombatWarsProyecto/src/Juego/Launcher/Launcher.java
@@ -1,8 +1,12 @@
 package Juego.Launcher;
 
+
+
 import Juego.Logger.logger;
+
 import Juego.VentanasLogIn.VentanaLogin;
 //import Juego.baseDeDatos.baseDeDatos;
+
 
 public class Launcher {
     public static void main(String[] args) {
@@ -12,7 +16,25 @@ public class Launcher {
 
         logger.LanzarLogger();
         //baseDeDatos.MeterDatos(); <- Este método ya se ha usado, no lo necesitamos otra vez.
+        String[] wombats = {"wombatNormal", "wombatGordo", "wombatOro", "wombatDiamante", "wombatTiempo", "wombatBomba", "wombatNuke", "reyWombat"};
+
+        mostrarDatosWombat(wombats, 0);
+
+        
     }
 
+    
+    
+     //Metodo recursivo para obtener los datos que hay metidos en el array datosWombat que representa todos los wombat de la BD.
+     public static void mostrarDatosWombat(String[] wombats, int indice) {
+        
 
+        if(indice != wombats.length) {
+            System.out.println(wombats[indice]);
+            mostrarDatosWombat(wombats, indice+1);
+        }
+    }
+  
+
+    
 }
